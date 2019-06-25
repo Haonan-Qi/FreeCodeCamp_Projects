28b8574ae706ae6a7cfbd1f890d28a38a4a61e63
Seven Qi <qhn2011011@gmail.com>
2019-06-25 15:22:42 +1000

    [Project Update]Phone Validator

---
+++ Javascript Algorithms And Data Structures Certification (300 hours)/Telephone Number Validator/TEL Validator.js
@@ -0,0 +1,16 @@
+function telephoneCheck(str) {
+    var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    return regex.test(str);
+}
+telephoneCheck("555-555-5555");
+
+
+
+
+//---------Better solution
+
+function telephoneCheck(str) {
+    var re = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})$/;
+    return re.test(str);
+}
+telephoneCheck("555-555-5555");
